f3c84d7e27f3359cafef98ddb376686081d822a2
root <root@ubuntu.(none)>
2015-04-07 17:01:43 +0800

    modify

--- debugger.go
+++ debugger.go
@@ -1,8 +1,9 @@
 package debugger
 
 import "runtime"
+import "fmt"
 
 func Where() string {
 	var _, file, line, _ = runtime.Caller(1)
-	return file + ":" + line
+	return fmt.Sprintf("%s:%d", file, line)
 }
